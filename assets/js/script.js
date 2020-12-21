21f320e72189ea3f880acf85166b555e84ff6bed
bamithprdxn <amithkumarb@prdxn.com>
2020-12-21 12:28:26 +0530

    code Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,21 @@
-/* Author: 
+function initMap(location) {
+   
+    // latitude and longitude of Mumbai
+    let mumbai = { lat: 19.0760, lng: 72.8777 };
+    let delhi = { lat: 28.7041, lng: 77.1025 };
 
-*/
+    // Map option
+    let options = {
+        zoom: 5,
+        center: mumbai,
+    };
 
+    // new Map
+    let map = new google.maps.Map(document.querySelector(".footer-right"), options);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // add Marker
+    let marker = new google.maps.Marker({
+        position: mumbai,
+        map: map
+    });
+}
